d25a7b7f671d007dec2f28be2275c40f16d3d704
Michael Ruiz <mruiz6@uwo.ca>
2022-11-02 23:21:38 -0400

    added get requests and reformated code

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-require('dotenv').config();
+const fs = require('fs');
+const csv = require('csv-parser');
 const express = require('express');
-const mongoose = require('mongoose');
-const musicRouter = require('routes/musicRouter')
 const app = express();
+const router = express.Router();
 const port = 3000;
 
-mongoose.connect(process.env.DATABASE_URL, {userNewUrlParser: true});
-const db = mongoose.connection
+let genres = [];
+let tracks = [];
+fs.createReadStream('lab3-data/genres.csv').pipe(csv()).on('data', (rows) => {genres.push(rows);});
+fs.createReadStream('lab3-data/raw_tracks.csv').pipe(csv()).on('data', (rows) => {tracks.push(rows);});
 
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
-
+app.use('/', express.static('static'));
 app.use(express.json());
 
-app.use('/music', musicRouter);
+app.use((req, res, next) => {
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
 
+// Get all genres
+router.get('/genres', async (req, res) => {
+    res.send(genres);
+});
+
+// Get specific genres
+router.get('/genres/:id', async (req, res) => {
+    const id = req.params.id
+    res.send(id);
+});
+
+// Get all tracks
+router.get('/tracks', async (req, res) => {
+    res.send(tracks);
+});
+
+app.use('/api/music', router)
 app.listen(port, () => console.log(`Server Started on Port ${port}`));
