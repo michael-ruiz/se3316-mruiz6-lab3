ae2cae1002273b4f5676647bbf1982ad9489fbb7
Michael Ruiz <mruiz6@uwo.ca>
2022-11-03 19:27:21 -0400

    imported all the csv files and changed the gitignore

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const router = express.Router();
 const port = 3000;
 
 let genres = [];
+let albums = [];
+let artists = [];
 let tracks = [];
 fs.createReadStream('lab3-data/genres.csv').pipe(csv()).on('data', (rows) => {genres.push(rows);});
+fs.createReadStream('lab3-data/raw_albums.csv').pipe(csv()).on('data', (rows) => {albums.push(rows);});
+fs.createReadStream('lab3-data/raw_artists.csv').pipe(csv()).on('data', (rows) => {artists.push(rows);});
 fs.createReadStream('lab3-data/raw_tracks.csv').pipe(csv()).on('data', (rows) => {tracks.push(rows);});
 
 app.use('/', express.static('static'));
 app.use(express.json());
 
+// Print out the request
 app.use((req, res, next) => {
     console.log(`${req.method} request for ${req.url}`);
     next();
